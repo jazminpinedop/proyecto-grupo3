fc20446bc71c636d405221bb9cf0575cc89027a8
Jazmin Pinedo <jj.pinedop@alum.up.edu.pe>
2020-11-23 15:52:32 -0500

    Nueva versión 2.0

---
+++ .vscode/linio/settings/development.py
@@ -0,0 +1,22 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+
+INSTALLED_APPS += [
+
+]
+
+MIDDLEWARE += []
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STRIPE_PUBLIC_KEY = ''
+STRIPE_SECRET_KEY = ''
